a4bdfcec08fceb8bc74f4918df73cd58df0080bc
Florian <Florian@FloStation.speedport.ip>
2016-11-25 01:12:23 +0100

    First Commit from exlipse

---
+++ core/src/main/java/com/flowai/app/core/App.java
@@ -0,0 +1,13 @@
+package com.flowai.app.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
